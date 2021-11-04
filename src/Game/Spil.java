a3f820b82aff86a4de27fe9c40c188ed13a80c8d
s205347 <s205347@student.dtu.dk>
2021-11-04 12:08:25 +0100

    man kan gætte bogstaver som sætningen indeholder

---
+++ src/Game/Spil.java
@@ -0,0 +1,36 @@
+package Game;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+//Denne klasse indeholder spillogikken
+public class Spil {
+
+    public static void main(String[] args) {
+        String hemmeligSætning = "wheel of fortune ";
+        Scanner scan;
+        scan = new Scanner(System.in);
+        String spillerGæt = " ";
+        boolean ikkeFærdig = true;
+
+        while (ikkeFærdig) {
+            ikkeFærdig = false;
+            for(char hemmeligtBogstav : hemmeligSætning.toCharArray()) {
+                if (spillerGæt.indexOf(hemmeligtBogstav) == -1) {
+                    System.out.print("*");
+                    ikkeFærdig = true;
+                } else {
+                    System.out.print(hemmeligtBogstav);
+                }
+
+            }
+            System.out.println("\nIndtast venligt det bogstav du gætter på");
+            String bogstav = scan.next();
+            spillerGæt += bogstav;
+        }
+        System.out.println("Tillykke du er færdig og har vundet spillet! ;)");
+
+    }
+
+
+}
